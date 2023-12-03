7cece386c48d8cb5b0b7308657c318f5635fca26
Heeje <tjdgmlwp34567@gmail.com>
2023-12-03 20:52:20 +0900

    Array Exam

---
+++ src/array/ArrayEx8.java
@@ -0,0 +1,51 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayEx8 {
+
+    public static void main(String[] args) {
+
+        /**
+         * 학생수를 입력하세요:3
+         * 1번 학생의 성적을 입력하세요:
+         * 국어 점수:10
+         * 영어 점수:20
+         * 수학 점수:30
+         * 2번 학생의 성적을 입력하세요:
+         * 국어 점수:10
+         * 영어 점수:10
+         * 수학 점수:10
+         * 3번 학생의 성적을 입력하세요:
+         * 국어 점수:20
+         * 영어 점수:20
+         * 수학 점수:20
+         * 1번 학생의 총점: 60, 평균: 20.0
+         * 2번 학생의 총점: 30, 평균: 10.0
+         * 3번 학생의 총점: 60, 평균: 20.0
+         */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("학생수를 입력하세요: ");
+        int count = sc.nextInt();
+        String[] subjects = {"국어", "영어", "수학"};
+
+        int[][] students = new int[count][subjects.length];
+        int sum = 0;
+
+        for(int i = 0; i < count; i++){
+            System.out.println((i+1)+ "번 학생의 성적을 입력하세요 : ");
+            for(int j = 0; j < students[i].length; j++){
+                students[i][j] = sc.nextInt();
+                System.out.println(subjects[j] + " 점수:" + students[i][j]);
+                sum += students[i][j];
+            }
+            double avg = (double) sum / subjects.length;
+            System.out.println((i+1) + "번 학생의 총점 : " + sum + ", 평균 : " + avg);
+        }
+
+
+
+
+    }
+}
---
+++ src/array/ProductProgram.java
@@ -0,0 +1,76 @@
+package array;
+
+import java.util.Scanner;
+
+public class ProductProgram {
+
+    public static void main(String[] args) {
+        /**
+         * 제약조건 : 상품은 10개 까지 등록
+         * 1. 상품 등록 | 2. 상품 목록 | 3. 종료
+         * 메뉴를 선택하세요:1
+         * 상품 이름을 입력하세요:JAVA
+         * 상품 가격을 입력하세요:10000
+         * 1. 상품 등록 | 2. 상품 목록 | 3. 종료
+         * 메뉴를 선택하세요:1
+         * 상품 이름을 입력하세요:SPRING
+         */
+
+        int maxProducts = 3;
+        String[] productName = new String[maxProducts];
+        int[] productPrices = new int[maxProducts];
+        int productCount = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("----------------------------------");
+            System.out.print("1. 상품 등록 | 2. 상품 목록 | 3. 종료\n메뉴를 선택하세요 : ");
+            int menu = sc.nextInt();
+            sc.nextLine();
+
+            if(menu == 1 && productCount >= maxProducts){
+                System.out.println("더이상 상품을 등록할 수 없습니다.");
+                System.out.println("메뉴 선택 화면으로 돌아갑니다.");
+                continue;
+            }
+
+            if(menu == 2 && productCount == 0){
+                System.out.println("등록된 상품이 없습니다.");
+                System.out.println("메뉴 선택 화면으로 돌아갑니다.");
+                continue;
+            }
+
+            switch (menu){
+                case 1 :
+
+                    System.out.print("상품 이름을 입력하세요 : ");
+                    productName[productCount] = sc.nextLine();
+
+                    System.out.print("상품 가격을 입력하세요 : ");
+                    productPrices[productCount] = sc.nextInt();
+
+                    productCount++;
+
+                    break;
+                case 2 :
+                    for(int i = 0; i < productCount; i++){
+                        System.out.println(productName[i] + " : " + productPrices[i] + "원");
+                    }
+                    continue;
+                case 3 :
+                    System.out.println("프로그램이 종료됩니다");
+                    return;
+                default :
+                    System.out.println("잘못된 메뉴를 선택하였습니다.");
+                    System.out.println("메뉴 선택 화면으로 돌아갑니다.");
+            }
+
+
+
+
+        }
+    }
+
+
+}
